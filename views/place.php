c75c8513dfff8d89ca347d09a97a6809f1888558
Brad Sawicki <git@bradsawicki.com>
2014-10-13 15:49:08 -0500

    Place updates

--- views/place.php
+++ views/place.php
@@ -1,18 +1,18 @@
 <section class="place section">
 
-  <div class="col-1-3">
-    <h2>The Date</h2>
+  <div class="col-1-3 col-place">
+    <h2>The Place</h2>
     <p>Our ceremony and reception will take place at the same location, so don?t stress about finding your way between the two.</p>
     <p>Prairie Production is a beautiful, airy photo studio on Randolph Street in Chicago, about a mile and a half south of our home.</p>
     <p>Street parking is available for all you designated drivers out there. You?re the real heroes.</p>
-    <p>Bonus: One of the only Banksy pieces in Chicago is just down the street</p>
+    <p class="bonus">Bonus: One of the only Banksy pieces in Chicago is just down the street</p>
   </div>
 
-  <div class="col-1-3">
+  <div class="col-1-3 col-location">
     <div class="location">
       <div class="icon-marker"></div>
-      <h3>Prairie Production</h3>
-      <p>1314 W. Randolph<br>
+      <h3 class="hotel-title">Prairie Production</h3>
+      <p class="hotel-address">1314 W. Randolph<br>
       Chicago, IL 60607<br>
       (312) 879-7499</p>
       <ul>
@@ -22,16 +22,18 @@
     </div>
   </div>
   
-  <div class="col-1-3">
+  <div class="col-1-3 col-reserve">
   
     <div class="reserved">
       <h2>Where to Stay</h2>
-      <p>We've reserved a block of rooms at</p>
-      <h4>Hotel Allegro</h4>
-      <p>171 W. Randolph<br>
-      Chicago, IL 60601<br>
-      (312) 236-0123</p>
-      <p><a href="" class="btn">Book a Room</a></p>
+      <div class="main-hotel">
+        <p class="sub-title">We've reserved a block of rooms at</p>
+        <h4 class="hotel-title">Hotel Allegro</h4>
+        <p class="hotel-address">171 W. Randolph<br>
+        Chicago, IL 60601<br>
+        (312) 236-0123</p>
+        <p><a href="" class="btn">Book a Room</a></p>
+      </div>
     </div>
     
     <div class="options">
