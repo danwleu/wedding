08d44fbbcfcb27b25d12cf7c723929bf1539c3f5
Brad Sawicki <git@bradsawicki.com>
2014-10-14 16:22:56 -0500

    Page wrapper

--- index.php
+++ index.php
@@ -1,15 +1,19 @@
-<?php include $_SERVER['DOCUMENT_ROOT'].'/views/header.php'; ?>
+<div class="page">
 
-<?php include $_SERVER['DOCUMENT_ROOT'].'/views/date.php'; ?>
+  <?php include $_SERVER['DOCUMENT_ROOT'].'/views/header.php'; ?>
+  
+  <?php include $_SERVER['DOCUMENT_ROOT'].'/views/date.php'; ?>
+  
+  <?php include $_SERVER['DOCUMENT_ROOT'].'/views/place.php'; ?>
+  
+  <?php include $_SERVER['DOCUMENT_ROOT'].'/views/couple.php'; ?>
+  
+  <?php include $_SERVER['DOCUMENT_ROOT'].'/views/rsvp.php'; ?>
+  
+  <?php include $_SERVER['DOCUMENT_ROOT'].'/views/party.php'; ?>
+  
+  <?php include $_SERVER['DOCUMENT_ROOT'].'/views/registry.php'; ?>
+  
+  <?php include $_SERVER['DOCUMENT_ROOT'].'/views/footer.php'; ?>
 
-<?php include $_SERVER['DOCUMENT_ROOT'].'/views/place.php'; ?>
-
-<?php include $_SERVER['DOCUMENT_ROOT'].'/views/couple.php'; ?>
-
-<?php include $_SERVER['DOCUMENT_ROOT'].'/views/rsvp.php'; ?>
-
-<?php include $_SERVER['DOCUMENT_ROOT'].'/views/party.php'; ?>
-
-<?php include $_SERVER['DOCUMENT_ROOT'].'/views/registry.php'; ?>
-
-<?php include $_SERVER['DOCUMENT_ROOT'].'/views/footer.php'; ?>
+</div>
