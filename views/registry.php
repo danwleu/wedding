1778c65dfa9950a4a324e8b855f6f15d52fe366b
Brad Sawicki <git@bradsawicki.com>
2014-10-16 10:37:56 -0500

    Registry links

--- views/registry.php
+++ views/registry.php
@@ -3,9 +3,9 @@
   <p>We&#8217;re on file at:</p>
   
   <ul class="registry-logos">
-    <li><a href="" target="_blank"><img src="/assets/images/logo-cb.png" alt="Crate & Barrel"></a></li>
-    <li><a href="" target="_blank"><img src="/assets/images/logo-cb2.png" alt="CB2"></a></li>
-    <li><a href="" target="_blank"><img src="/assets/images/logo-sur.png" alt="Surlatable"></a></li>
-    <li><a href="" target="_blank"><img src="/assets/images/logo-bbb.png" alt="Bed, Bath & Beyond"></a></li>
+    <li><a href="http://www.crateandbarrel.com/Gift-Registry/daniel-leu-and-katie-brandt/r5216290" target="_blank"><img src="/assets/images/logo-cb.png" alt="Crate & Barrel"></a></li>
+    <li><a href="http://www.cb2.com/Gift-Registry/daniel-leu-and-katie-brandt/r5223062" target="_blank"><img src="/assets/images/logo-cb2.png" alt="CB2"></a></li>
+    <li><a href="http://www.surlatable.com/registry/giftRegistryList.jsp?id=200989833903" target="_blank"><img src="/assets/images/logo-sur.png" alt="Surlatable"></a></li>
+    <li><a href="http://www.bedbathandbeyond.com/store/giftregistry/view_registry_guest.jsp?pwsToken=&eventType=Wedding&registryId=541472181&pwsurl=" target="_blank"><img src="/assets/images/logo-bbb.png" alt="Bed, Bath & Beyond"></a></li>
   </ul>  
 </section>
